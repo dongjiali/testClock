8c0571b759bec1c860878f0b733d6b8f05565073
liuchenguang <liuchenguangfree.gmail.com>
2013-06-19 13:40:42 +0800

    Initial Commit

---
+++ testClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testClock
+//
+//  Created by l_ch_g on 13-6-19.
+//  Copyright (c) 2013年 刘晨光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testClock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testClock
+//
+//  Created by l_ch_g on 13-6-19.
+//  Copyright (c) 2013年 刘晨光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
